0aa77ca9adb85f7ab8145b3eccaac7a9a1ce6c3b
Prathamesh <pdpatil1006@gmail.com>
2025-12-06 17:39:07 +0530

    Added Booking and User Entity class

---
+++ airBnbApp/src/main/java/com/portfolio/projects/airBnbApp/entity/User.java
@@ -0,0 +1,31 @@
+package com.portfolio.projects.airBnbApp.entity;
+
+import com.portfolio.projects.airBnbApp.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "app_user") // changing table name to app_user because
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String name;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+}
