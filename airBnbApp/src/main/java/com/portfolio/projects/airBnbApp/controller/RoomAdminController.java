4f4a7d063b41c2745babe4bf1ca5a992e59ee2fa
Prathamesh <pdpatil1006@gmail.com>
2026-01-18 23:52:18 +0530

    Add Room management functionality with controller, service, and implementation

---
+++ airBnbApp/src/main/java/com/portfolio/projects/airBnbApp/controller/RoomAdminController.java
@@ -0,0 +1,42 @@
+package com.portfolio.projects.airBnbApp.controller;
+
+import com.portfolio.projects.airBnbApp.dto.RoomDto;
+import com.portfolio.projects.airBnbApp.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/hotels/{hotelId}/rooms")
+@RequiredArgsConstructor
+public class RoomAdminController {
+
+    private final RoomService roomService;
+
+    @PostMapping
+    public ResponseEntity<RoomDto> createNewRoom(@PathVariable Long hotelId, @RequestBody RoomDto roomDto){
+        RoomDto room = roomService.createNewRoom(hotelId, roomDto);
+        return new ResponseEntity<>(room, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<java.util.List<RoomDto>> getAllRoomsInHotel(@PathVariable Long hotelId){
+        List<RoomDto> rooms = roomService.getAllRoomsInHotel(hotelId);
+        return new ResponseEntity<>(rooms, HttpStatus.OK);
+    }
+
+    @GetMapping("/{roomId}")
+    public ResponseEntity<RoomDto> getRoomById(@PathVariable Long hotelId, @PathVariable Long roomId){
+        RoomDto room = roomService.getRoomById(roomId);
+        return new ResponseEntity<>(room, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{roomId}")
+    public ResponseEntity<Void> deleteRoomById(@PathVariable Long hotelId, @PathVariable Long roomId){
+        roomService.deleteRoomById(roomId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
