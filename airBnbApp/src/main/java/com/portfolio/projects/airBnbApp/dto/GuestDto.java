c04c97508caaff43d7f4f143729752d195d37937
Prathamesh <pdpatil1006@gmail.com>
2026-06-27 13:32:35 +0530

    Refactor GuestDto to use UserDto and add gender and dateOfBirth fields to UserDto; update security configuration to require authentication for user endpoints

--- airBnbApp/src/main/java/com/portfolio/projects/airBnbApp/dto/GuestDto.java
+++ airBnbApp/src/main/java/com/portfolio/projects/airBnbApp/dto/GuestDto.java
@@ -1,13 +1,13 @@
 package com.portfolio.projects.airBnbApp.dto;
 
-import com.portfolio.projects.airBnbApp.entity.User;
+import com.portfolio.projects.airBnbApp.dto.UserDto;
 import com.portfolio.projects.airBnbApp.entity.enums.Gender;
 import lombok.Data;
 
 @Data
 public class GuestDto {
     private Long id;
-    private User user;
+    private UserDto user;
     private String name;
     private Gender gender;
     private Integer age;
