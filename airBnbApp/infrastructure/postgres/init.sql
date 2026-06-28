2e06bbdd6139a43ff864d250ad3de4ffe1e665c8
Prathamesh <pdpatil1006@gmail.com>
2026-06-28 20:08:57 +0530

    Update configurations to use separate databases

---
+++ airBnbApp/infrastructure/postgres/init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE user_service_db;
+CREATE DATABASE auth_service_db;
+CREATE DATABASE inventory_service_db;
+CREATE DATABASE property_service_db;
+CREATE DATABASE booking_service_db;
+CREATE DATABASE search_service_db;
