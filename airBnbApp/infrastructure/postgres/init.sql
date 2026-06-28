2db71cbfd66511aa05f813b655e68fb5ee9405ac
Prathamesh <pdpatil1006@gmail.com>
2026-06-28 20:41:34 +0530

    Clean up config files: fix inventory auth, fix booking and payment fallbacks, add payment db

--- airBnbApp/infrastructure/postgres/init.sql
+++ airBnbApp/infrastructure/postgres/init.sql
@@ -4,3 +4,4 @@ CREATE DATABASE inventory_service_db;
 CREATE DATABASE property_service_db;
 CREATE DATABASE booking_service_db;
 CREATE DATABASE search_service_db;
+CREATE DATABASE payment_service_db;
